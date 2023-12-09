8dda80da158b201f72791fdcc47c8b3c4d2b8802
Marcio Costa <marcio-costa38@hotmail.com>
2023-12-08 22:50:22 -0300

    adicionando a conexão com a API JDBC do java para conectar ao DB.

---
+++ Carros/src/main/java/br/com/livro/domain/BaseDAD.java
@@ -0,0 +1,36 @@
+package br.com.livro.domain;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BaseDAD {
+	public BaseDAD() {
+		try {
+			//Necessario para utilizar o driver JDBC do MySql
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			//Erro de driver JDBC (adicione o driver .jar do MySql em /WEB-INF/lib)
+			e.printStackTrace();
+		}
+	}
+	
+	protected Connection getConnection() throws SQLException {
+		//URL de conexão com o banco de dados
+		String url = "jdbc:mysql://localhost/livro";
+		
+		//Conecta utilizando a URL, usuario e senha.
+		Connection conn = DriverManager.getConnection(url, "livro", "livro123");
+		return conn;
+	}
+	
+	public static void main(String[] args) throws SQLException {
+		BaseDAD db = new BaseDAD();
+		
+		//testa a conexao.
+		Connection conn = db.getConnection();
+		System.out.println(conn);
+	
+		
+	}
+}
