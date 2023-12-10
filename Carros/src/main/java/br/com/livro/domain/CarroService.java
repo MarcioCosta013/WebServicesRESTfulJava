633cebe57dec7b8313dc421fd328dcb565be7532
Marcio Costa <marcio-costa38@hotmail.com>
2023-12-10 18:42:19 -0300

    adicionei a classe CarroService e a CarroTest...

--- Carros/src/main/java/br/com/livro/domain/CarroService.java
+++ Carros/src/main/java/br/com/livro/domain/CarroService.java
@@ -19,5 +19,47 @@ public class CarroService {
 	}
 	
 	//Busca um carro pelo id
+	public Carro getCarro(Long id) {
+		try {
+			return db.getCarroById(id);
+		} catch (SQLException e) {
+			return null;
+		}
+	}
 	
+	//Deleta o carro pelo id
+	public boolean delete(Long id) {
+		try {
+			return db.delete(id);
+		} catch (SQLException e) {
+			return false;
+		}
+	}
+	
+	//Salva ou atualiza o carro
+	public boolean save(Carro carro) {
+		try {
+			db.save(carro);
+			return true;
+		} catch (SQLException e) {
+			return false;
+		}
+	}
+	
+	//Busca o carro pelo nome
+	public List<Carro> findByName(String name) {
+		try {
+			return db.findByName(name);
+		} catch (SQLException e) {
+			return null;
+		}
+	}
+	
+	public List<Carro> findByTipo(String tipo){
+		try {
+			return db.findByTipo(tipo);
+		} catch (SQLException e) {
+			return null;
+		}
+	}
 }
